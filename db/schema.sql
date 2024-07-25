20894462c4900ea5f81e798bb195863db05ac029
Bryson <brysob7617@gmail.com>
2024-07-24 22:20:54 -0400

    added functions to view departments and roles and also added a foreign key reference to departmets in roles table

--- db/schema.sql
+++ db/schema.sql
@@ -12,7 +12,9 @@ CREATE TABLE roles (
     id SERIAL PRIMARY KEY,
     title VARCHAR(30) UNIQUE NOT NULL,
     salary DECIMAL NOT NULL,
-    department_id INTEGER NOT NULL
+    department_id INTEGER NOT NULL,
+    FOREIGN KEY (department_id)
+    REFERENCES departments(id)
 );
 
 CREATE TABLE employees (
--- server.js
+++ server.js
@@ -17,22 +17,62 @@ const pool = new Pool(
 
 pool.connect();
 
-init();
 
 const init = function() {
     inquirer
-        .prompt({
-            type: 'list',
-            name: 'mainList',
-            message: 'What would you like to do?',
-            choices: [
-                'View All Employees',
-                'Add Employee',
-                'Update Employee Role',
-                'View All Roles',
-                'Add Role',
-                'View All Departments',
-                'Add Department',
-            ],
-        })
+    .prompt({
+        type: 'list',
+        name: 'mainList',
+        message: 'What would you like to do?',
+        choices: [
+            'View All Departments',
+            'View All Roles',
+            'View All Employees',
+            'Add Department',
+            'Add Role',
+            'Add Employee',
+            'Update Employee Role',
+        ],
+    })
 };
+
+const viewAllDepartments = function() {
+    pool.query(`
+        SELECT * FROM departments
+        `, function (err, {rows}) {
+        console.table(rows)
+    })
+};
+
+const viewAllRoles = function() {
+    pool.query(`
+        SELECT * roles.title, roles.id, departments.name, roles.salary 
+        FROM roles 
+        JOIN departments 
+        ON roles.department_id = departments.id
+        `, function (err, {rows}) {
+        console.table(rows)
+    })
+};
+
+const viewAllEmployees = function() {
+    
+};
+
+const addDepartment = function() {
+    
+};
+
+const addRole = function() {
+    
+};
+
+const addEmployee = function() {
+    
+};
+
+const updateEmployeeRole = function() {
+    
+};
+
+init();
